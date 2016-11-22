7e5ff4896533728cc8c7a0321158df0b038cb74c
Raphael Assenat <raph@raphnet.net>
2016-11-22 16:43:38 -0500

    Initial commit

---
+++ src/version.h
@@ -0,0 +1,8 @@
+#ifndef _version_h__
+#define _version_h__
+
+#define PLUGIN_NAME "raphnetraw for Project64"
+#define PLUGIN_VERSION	0x000900
+#define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
+
+#endif // _version_h__
