b6ecde8107a68c9e9ea9ad8d81cf6e463abc9d80
Raphael Assenat <raph@raphnet.net>
2016-11-28 16:11:41 -0500

    Increase version to 0.9.4

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define _version_h__
 
 #define PLUGIN_NAME "raphnetraw for Project64"
-#define PLUGIN_VERSION	0x000903
+#define PLUGIN_VERSION	0x000904
 #define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
 
 #endif // _version_h__
