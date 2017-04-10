3bb3ed47b1b959835dcc1655be4160b1e4ced314
Raphael Assenat <raph@raphnet.net>
2017-04-10 18:18:42 -0400

    Version 1.0.2

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define _version_h__
 
 #define PLUGIN_NAME "raphnetraw for Project64"
-#define PLUGIN_VERSION	0x010000
+#define PLUGIN_VERSION	0x010002
 #define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
 
 #endif // _version_h__
